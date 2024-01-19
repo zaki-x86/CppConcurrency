a55c83fd497393b34b11af152244b30badca6891
zaki-x86 <mamadovzicx@gmail.com>
2024-01-19 06:27:59 +0200

    deadlock example FIXED

--- deadlock.cpp
+++ deadlock.cpp
@@ -1,50 +1,39 @@
+/**
+ * Two shoppers adding garlic and potatoes to a shared notepad
+ * deadlock example
+ */
 #include <thread>
-#include <cstdio>
-#include <vector>
 #include <mutex>
 
-// 2 Threads end up in a deadlock example
-// Thread worker1 tries to lock resource_mutex, and then to unlock it, it needs to lock resource_mutex2
-// Thread worker2 tries to lock resource_mutex2, and then to unlock it, it needs to lock resource_mutex
-// None of the threads is able to unlock their mutexes, so they are stuck in a deadlock
+unsigned int garlic_count = 0;
+unsigned int potato_count = 0;
+std::mutex pencil;
 
-std::mutex resource_mutex; 
-int resource = 0;
-std::mutex resource_mutex2;
-int times = 1000000;
+void add_garlic() {
+    pencil.lock();
+    garlic_count++;
+    pencil.unlock();
+}
 
-void increment_worker() {
-    while (times > 0){
-        resource_mutex.lock();
-        resource_mutex2.lock();
-        --times;
-        resource_mutex2.unlock();
-        resource++;
-        resource_mutex.unlock();
+void add_potato() {
+    pencil.lock();
+    potato_count++;
+    add_garlic();
+    pencil.unlock();
+}
+
+void shopper() {
+    for (int i=0; i<10000; i++) {
+        add_garlic();
+        add_potato();
     }
-    printf("Worker done\n");
 }
 
-void decrement_worker() {
-    resource_mutex2.lock();
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    resource_mutex.lock();
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    resource_mutex.unlock();
-    resource_mutex2.unlock();
-}
-
-int main(int argc, char const *argv[])
-{
-    std::thread worker1(increment_worker);
-    std::thread worker2(decrement_worker);
-    
-    printf("Resource: %d\n", resource);
-
-    worker1.join();
-    worker2.join();
-
-    printf("Resource: %d\n", resource);
-
-    return 0;
+int main() {
+    std::thread barron(shopper);
+    std::thread olivia(shopper);
+    barron.join();
+    olivia.join();
+    printf("We should buy %u garlic.\n", garlic_count);
+    printf("We should buy %u potatoes.\n", potato_count);
 }
