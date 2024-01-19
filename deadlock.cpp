346d0b40b253a8a0b006babe99400a4d910f979f
zaki-x86 <mamadovzicx@gmail.com>
2024-01-19 05:43:07 +0200

    deadlock example:
    a thread trying to lock a resource twice

---
+++ deadlock.cpp
@@ -0,0 +1,44 @@
+#include <thread>
+#include <cstdio>
+#include <vector>
+#include <mutex>
+
+// Mutex
+// protects a resource from being accessed simultaneously by multiple thread
+// by locking the resource, only one thread can access it at a time during atomic operations.
+
+std::mutex resource_mutex; 
+int resource = 0;
+
+void increment_worker() {
+    int times = 10000;
+    while (times > 0){
+        resource_mutex.lock();
+        resource_mutex.lock();
+        resource++;
+        resource_mutex.unlock();
+        resource_mutex.unlock();
+        --times;
+    }
+    printf("Worker done\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    const size_t threads_num = 20;
+    std::vector<std::thread> workers;
+    for (size_t i = 0; i < threads_num; i++)
+    {
+        workers.push_back(std::thread(increment_worker));
+    }
+    
+
+    for (auto &&worker : workers)
+    {
+        worker.join();
+    }
+    
+    printf("Resource: %d\n", resource);
+
+    return 0;
+}
